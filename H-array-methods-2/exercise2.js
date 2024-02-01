2c12c89271bd10dea9020a9af7054ff841e91ed5
Ruchi Jangra <ruchijangra2022@gmail.com>
2024-02-02 03:05:27 +0530

    done

--- H-array-methods-2/exercise2.js
+++ H-array-methods-2/exercise2.js
@@ -7,7 +7,16 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  // Check if the input string is not empty
+  if (str.length > 0) {
+    // Capitalize the first letter and concatenate with the rest of the string
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  } else {
+    // If the input string is empty, return an empty string
+    return "";
+  }
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
